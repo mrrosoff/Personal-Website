80facd7439432a968a330a8634e8e4284e7fb817
Max Rosoff <me@maxrosoff.com>
2020-11-27 18:35:27 -0700

    Readd Some Files and Stuff

--- site/index.js
+++ site/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from "./components/App";
 
+import style from "./static/styles/global.scss"
+
 ReactDOM.render(<App/>, document.getElementById('root'));
