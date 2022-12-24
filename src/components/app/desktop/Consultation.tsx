cd674f3cce57cae5c3d1323c810caba9067bff9d
Max Rosoff <me@maxrosoff.com>
2022-12-24 03:04:35 +0000

    Progress On Consulting Typing Animation

--- src/components/app/desktop/Consultation.tsx
+++ src/components/app/desktop/Consultation.tsx
@@ -1,5 +1,51 @@
+import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+
 const Consultation = (props: any) => {
-    return <> </>;
+    const [mainText, setMainText] = useState<string>("");
+    const [secondaryText, setSecondaryText] = useState<string>("");
+
+    useEffect(() => {
+        // set up text to print, each item in array is new line
+        const fullMainText = "Consulting";
+        const fullSecondaryText =
+            "If you have a need for any variety of tech services, I could be your solution.";
+
+        const typingSpeed = 250;
+        const secondaryTextDelay = 5000;
+
+        const mainTextInterval = setInterval(() => {
+            const fullMainIndex = fullMainText.indexOf(mainText) || 0 + mainText.length + 1;
+            console.log(mainText, fullMainIndex)
+            if (fullMainIndex > fullMainText.length) {
+                clearInterval(mainTextInterval);
+            }
+            setMainText(mainText + fullMainText[fullMainIndex]);
+        }, typingSpeed);
+
+        const secondaryTextTimeout = setTimeout(() => {
+            const secondaryTextInterval = setInterval(() => {
+                const fullSecondaryIndex =
+                    fullSecondaryText.indexOf(mainText) + mainText.length + 1;
+                if (fullSecondaryIndex > fullSecondaryText.length) {
+                    clearInterval(secondaryTextInterval);
+                }
+                setSecondaryText(mainText + fullSecondaryText[fullSecondaryIndex]);
+            }, typingSpeed);
+        }, secondaryTextDelay);
+
+        return () => {
+            clearTimeout(secondaryTextTimeout);
+            clearInterval(mainTextInterval);
+        };
+    }, []);
+
+    return (
+        <Box>
+            <Typography>{mainText}</Typography>
+            <Typography>{secondaryText}</Typography>
+        </Box>
+    );
 };
 
 export default Consultation;
