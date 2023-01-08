034b8ec46bf33b699981415d34958a144cfffe1a
Max Rosoff <me@maxrosoff.com>
2023-01-08 19:51:00 +0000

    Fleshed out Consultation Section More

--- src/components/app/SocialButtons.tsx
+++ src/components/app/SocialButtons.tsx
@@ -8,94 +8,94 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
 const SocialList = [
-	{
-		name: "Resume",
-		url: "https://bit.ly/rosoff-resume",
-		icon: DescriptionIcon
-	},
-	{
-		name: "LinkedIn",
-		url: "https://www.linkedin.com/in/max-rosoff",
-		icon: LinkedInIcon
-	},
-	{
-		name: "GitHub",
-		url: "https://www.github.com/mrrosoff",
-		icon: GitHubIcon
-	},
-	{
-		name: "Facebook",
-		url: "https://www.facebook.com/maxr.rosoff",
-		icon: FacebookIcon
-	},
-	{
-		name: "Instagram",
-		url: "https://www.instagram.com/thenameismr.r/",
-		icon: InstagramIcon
-	},
-	{
-		name: "Twitter",
-		url: "https://twitter.com/MrRosoff",
-		icon: TwitterIcon
-	}
+    {
+        name: "Resume",
+        url: "https://bit.ly/rosoff-resume",
+        icon: DescriptionIcon
+    },
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/max-rosoff",
+        icon: LinkedInIcon
+    },
+    {
+        name: "GitHub",
+        url: "https://www.github.com/mrrosoff",
+        icon: GitHubIcon
+    },
+    {
+        name: "Facebook",
+        url: "https://www.facebook.com/maxr.rosoff",
+        icon: FacebookIcon
+    },
+    {
+        name: "Instagram",
+        url: "https://www.instagram.com/thenameismr.r/",
+        icon: InstagramIcon
+    },
+    {
+        name: "Twitter",
+        url: "https://twitter.com/MrRosoff",
+        icon: TwitterIcon
+    }
 ];
 
-export const DesktopSocialButtonList = () => {
-	return (
-		<Box p={2} display={"flex"} flexWrap={"wrap"}>
-			{SocialList.map((socialDetails, index) => (
-				<Box key={index} mr={1}>
-					<SocialButton
-						href={socialDetails.url}
-						icon={socialDetails.icon}
-						text={socialDetails.name}
-					/>
-				</Box>
-			))}
-		</Box>
-	);
+export const DesktopSocialButtonList = (props: { sx?: any }) => {
+    return (
+        <Box p={2} sx={props.sx} display={"flex"} flexWrap={"wrap"}>
+            {SocialList.map((socialDetails, index) => (
+                <Box key={index} mr={1}>
+                    <SocialButton
+                        href={socialDetails.url}
+                        icon={socialDetails.icon}
+                        text={socialDetails.name}
+                    />
+                </Box>
+            ))}
+        </Box>
+    );
 };
 
 export const MobileSocialButtonList = () => {
-	return (
-		<Grid
-			container
-			direction={"column"}
-			spacing={1}
-			justifyContent={"center"}
-			alignItems={"center"}
-			alignContent={"center"}
-		>
-			{SocialList.map((socialDetails, index) => (
-				<Grid item key={index}>
-					<SocialButton
-						href={socialDetails.url}
-						icon={socialDetails.icon}
-						text={socialDetails.name}
-					/>
-				</Grid>
-			))}
-		</Grid>
-	);
+    return (
+        <Grid
+            container
+            direction={"column"}
+            spacing={1}
+            justifyContent={"center"}
+            alignItems={"center"}
+            alignContent={"center"}
+        >
+            {SocialList.map((socialDetails, index) => (
+                <Grid item key={index}>
+                    <SocialButton
+                        href={socialDetails.url}
+                        icon={socialDetails.icon}
+                        text={socialDetails.name}
+                    />
+                </Grid>
+            ))}
+        </Grid>
+    );
 };
 
 const SocialButton = (props: any) => {
-	const Icon = props.icon;
-	return (
-		<Button
-			className={"social-button"}
-			href={props.href}
-			target={"_blank"}
-			rel={"noopener"}
-			size={"large"}
-			{...props}
-		>
-			<Box sx={{ display: "flex", alignItems: "center", color: "white" }}>
-				<Icon />
-				<Box pl={2}>
-					<Typography>{props.text}</Typography>
-				</Box>
-			</Box>
-		</Button>
-	);
+    const Icon = props.icon;
+    return (
+        <Button
+            className={"social-button"}
+            href={props.href}
+            target={"_blank"}
+            rel={"noopener"}
+            size={"large"}
+            {...props}
+        >
+            <Box sx={{ display: "flex", alignItems: "center", color: "white" }}>
+                <Icon />
+                <Box pl={2}>
+                    <Typography>{props.text}</Typography>
+                </Box>
+            </Box>
+        </Button>
+    );
 };
--- src/components/app/desktop/Consultation.tsx
+++ src/components/app/desktop/Consultation.tsx
@@ -1,5 +1,6 @@
-import { Box, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Box, Skeleton, Typography } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import { DesktopSocialButtonList } from "../SocialButtons";
 
 const Consultation = (_props: any) => {
     const [mainText, setMainText] = useState<string>("");
@@ -8,10 +9,13 @@ const Consultation = (_props: any) => {
     const [visibleCursor, setVisibleCursor] = useState<boolean>(true);
     const [mainActiveCursor, setMainActiveCursor] = useState<boolean>(true);
 
-    const typingSpeed = 125;
-    const startDelay = 1000;
-    const intermissionDelay = 1200;
-    const secondStartDelay = 5500;
+    const [skeletonElements, setSkeletonElements] = useState<any[]>([]);
+
+    const typingSpeed = 100;
+    const startDelay = 800;
+    const intermissionDelay = 1000;
+    const secondStartDelay = 4000;
+    const blinkInProjectsDelay = 6500;
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -40,7 +44,7 @@ const Consultation = (_props: any) => {
     useEffect(() => {
         const timeout = setTimeout(() => {
             const fullSecondaryText =
-                "I provide a variety of consulting services, on and off the web.";
+                "I provide a variety of consulting services, for a technical project of any scale. Contact me using the links below for more information.";
             let i = 0;
             const interval = setInterval(() => {
                 const nextLetter = fullSecondaryText[i];
@@ -54,6 +58,30 @@ const Consultation = (_props: any) => {
         return () => clearTimeout(timeout);
     }, []);
 
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            let i = 0;
+            const interval = setInterval(() => {
+                console.log(i);
+                setSkeletonElements((skeletonElements) => [
+                    ...skeletonElements,
+                    <Skeleton
+                        key={i}
+                        variant="rectangular"
+                        width={500}
+                        height={500}
+                        sx={{ ml: 4 }}
+                    />
+                ]);
+                i++;
+                if (i > 3) {
+                    clearInterval(interval);
+                }
+            }, 350);
+        }, blinkInProjectsDelay);
+        return () => clearTimeout(timeout);
+    }, []);
+
     return (
         <Box display={"flex"} flexDirection={"column"}>
             <Box display={"flex"} alignItems={"center"} sx={{ height: 250 }}>
@@ -61,7 +89,7 @@ const Consultation = (_props: any) => {
                 <Box
                     id={"cursor"}
                     ml={mainText ? 2 : 0}
-                    width={50}
+                    width={45}
                     height={120}
                     sx={{
                         visibility: mainActiveCursor && visibleCursor ? "visible" : "hidden",
@@ -81,6 +109,12 @@ const Consultation = (_props: any) => {
                         background: "#FFFFFF"
                     }}
                 />
+            </Box>
+            <Box display={"flex"} alignItems={"center"} sx={{ ml: -4, mt: -2 }}>
+                {skeletonElements}
+            </Box>
+            <Box display={"flex"} alignItems={"center"} sx={{ ml: -2, mt: 6 }}>
+                <DesktopSocialButtonList sx={{ p: 0 }} />
             </Box>
         </Box>
     );
