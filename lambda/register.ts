0036feb0b39a742e868e037c5dcb6472b6ac3171
Max Rosoff <me@maxrosoff.com>
2025-05-03 19:29:35 -0700

    Add Success Code To Verify Calls Work

--- lambda/register.ts
+++ lambda/register.ts
@@ -1,3 +1,8 @@
-import { APIGatewayEvent } from "aws-lambda";
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export const handler = async (event: APIGatewayEvent) => {};
+export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+    return {
+        statusCode: 200,
+        body: "Email Registered Successfully."
+    };
+};
