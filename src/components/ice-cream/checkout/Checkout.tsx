8ecded57269afadf2ea4317bdb4a54f657b4e3b5
Max Rosoff <me@maxrosoff.com>
2025-11-15 23:44:14 -0800

    Cleanup A Bunch Of Information

--- src/components/ice-cream/checkout/Checkout.tsx
+++ src/components/ice-cream/checkout/Checkout.tsx
@@ -1,14 +1,10 @@
-import { ChangeEvent, Dispatch, FormEvent, SetStateAction, useMemo, useState } from "react";
+import { FormEvent, MouseEventHandler, useMemo, useState } from "react";
 
 import axios from "axios";
-import { Box, Button, CircularProgress, TextField, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { Appearance, loadStripe } from "@stripe/stripe-js";
-import {
-    CheckoutProvider,
-    PaymentElement,
-    StripeCheckoutValue,
-    useCheckout
-} from "@stripe/react-stripe-js/checkout";
+import { LinkAuthenticationElement, PaymentElement } from "@stripe/react-stripe-js";
+import { CheckoutProvider, useCheckout } from "@stripe/react-stripe-js/checkout";
 
 import ClaconFont from "../../../assets/fonts/clacon.ttf";
 import { API_URL } from "../../App";
@@ -17,93 +13,22 @@ const stripePublishableApiKey =
     "pk_live_51SSn4jGZZEzkLsbifOmvMvPB5xo33fgFS19ejvNuOibMMPHFu3ixt00c2nbCn4EPiIXWXvJvH1t3AZLXJE3dIrKz00aPsF6Dt2";
 const stripeLoader = loadStripe(stripePublishableApiKey);
 
-const validateEmail = async (email: string, checkout: StripeCheckoutValue) => {
-    const updateResult = await checkout.updateEmail(email);
-    const isValid = updateResult.type !== "error";
-
-    return { isValid, message: !isValid ? updateResult.error.message : null };
-};
-
-type EmailInputProps = {
-    email: string;
-    setEmail: Dispatch<SetStateAction<string>>;
-    error: string | null;
-    setError: Dispatch<SetStateAction<string | null>>;
-};
-
-const EmailInput = (props: EmailInputProps) => {
-    const state = useCheckout();
-    if (state.type === "loading" || state.type === "error") {
-        return;
-    }
-
-    const handleBlur = async () => {
-        if (!props.email) {
-            return;
-        }
-
-        const { isValid, message } = await validateEmail(props.email, state.checkout);
-        if (!isValid) {
-            props.setError(message);
-        }
-    };
-
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        props.setError(null);
-        props.setEmail(e.target.value);
-    };
-
-    return (
-        <Box display={"flex"}>
-            <Typography>Email</Typography>
-            <TextField
-                type="text"
-                value={props.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={!!props.error}
-            />
-            {props.error && <Typography>{props.error}</Typography>}
-        </Box>
-    );
-};
-
 const CheckoutForm = () => {
-    const [email, setEmail] = useState("");
-    const [emailError, setEmailError] = useState<string | null>(null);
     const [message, setMessage] = useState<string | null>(null);
     const [isLoading, setIsLoading] = useState(false);
 
     const state = useCheckout();
 
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
+    const handleSubmit = async () => {
         if (state.type === "loading" || state.type === "error") {
             return;
         }
 
         setIsLoading(true);
-
-        const { isValid, message } = await validateEmail(email, state.checkout);
-        if (!isValid) {
-            setEmailError(message);
-            setMessage(message);
-            setIsLoading(false);
-            return;
-        }
-
         const confirmResult = await state.checkout.confirm();
-
-        // This point will only be reached if there is an immediate error when
-        // confirming the payment. Otherwise, your customer will be redirected to
-        // your `return_url`. For some payment methods like iDEAL, your customer will
-        // be redirected to an intermediate site first to authorize the payment, then
-        // redirected to the `return_url`.
         if (confirmResult.type === "error") {
             setMessage(confirmResult.error.message);
         }
-
         setIsLoading(false);
     };
 
@@ -113,21 +38,23 @@ const CheckoutForm = () => {
 
     return (
         <Box>
-            <h2>Checkout</h2>
-            <form onSubmit={handleSubmit}>
-                <PaymentElement />
-                <Button
-                    color={"primary"}
-                    variant={"contained"}
-                    disabled={isLoading || state.type === "loading"}
-                    sx={{ width: "100%", mt: 2 }}
-                    type="submit"
-                    loading={isLoading || state.type === "loading"}
-                >
-                    Pay {state.type === "success" ? state.checkout.total.total.amount : ""}
-                </Button>
-                {message && <Box>{message}</Box>}
-            </form>
+            <Typography variant={"h1"}>Checkout</Typography>
+            <Typography variant={"h2"}>Contact Info</Typography>
+            <LinkAuthenticationElement />
+            <Typography variant={"h2"}>Payment</Typography>
+            <PaymentElement />
+            <Button
+                color={"primary"}
+                variant={"contained"}
+                disabled={isLoading || state.type === "loading"}
+                sx={{ width: "100%", mt: 2 }}
+                type="submit"
+                loading={isLoading || state.type === "loading"}
+                onClick={handleSubmit}
+            >
+                Pay {state.type === "success" ? state.checkout.total.total.amount : ""}
+            </Button>
+            {message && <Box>{message}</Box>}
         </Box>
     );
 };
